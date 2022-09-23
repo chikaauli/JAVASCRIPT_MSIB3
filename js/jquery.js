60f5b84e5cd0dd13b6af91ef07a0d76ca4a7c78b
chikaauli <chikaauliazahwa@gmail.com>
2022-09-23 18:04:46 +0700

    Tugas7_js_ChikaAuliaZahwa_STT-NF_Nasrul

---
+++ js/jquery.js
@@ -0,0 +1,221 @@
+$("document").ready(function () {
+    // Hewan Unggas
+    $("#ayam").click(function () {
+        $("#ayam-img").attr({
+            'src': "img/ayam.png"
+        });
+        $("#ayam-text").css({
+            'top': "75%"
+        })
+        $("#ayam-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ayam.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#bebek").click(function () {
+        $("#bebek-img").attr({
+            'src': "img/bebek.png"
+        });
+        $("#bebek-text").css({
+            'top': "75%"
+        })
+        $("#bebek-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/bebek.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#burung").click(function () {
+        $("#burung-img").attr({
+            'src': "img/burungbeo.png"
+        });
+        $("#burung-text").css({
+            'top': "75%"
+        })
+        $("#burung-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/beo.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#angsa").click(function () {
+        $("#angsa-img").attr({
+            'src': "img/angsa.png"
+        });
+        $("#angsa-text").css({
+            'top': "75%"
+        })
+        $("#angsa-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/angsa.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#pelikan").click(function () {
+        $("#pelikan-img").attr({
+            'src': "img/pelikan.png"
+        });
+        $("#pelikan-text").css({
+            'top': "75%"
+        })
+        $("#pelikan-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/pelikan.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+
+
+    // Hewan ternak
+    $("#sapi").click(function () {
+        $("#sapi-img").attr({
+            'src': "img/sapi.png"
+        });
+        $("#sapi-text").css({
+            'top': "75%"
+        })
+        $("#sapi-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/sapi.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#kambing").click(function () {
+        $("#kambing-img").attr({
+            'src': "img/kambing.png"
+        });
+        $("#kambing-text").css({
+            'top': "75%"
+        })
+        $("#kambing-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/kambing.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#kerbau").click(function () {
+        $("#kerbau-img").attr({
+            'src': "img/kerbau.png"
+        });
+        $("#kerbau-text").css({
+            'top': "75%"
+        })
+        $("#kerbau-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/kerbau.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#domba").click(function () {
+        $("#domba-img").attr({
+            'src': "img/domba.png"
+        });
+        $("#domba-text").css({
+            'top': "75%"
+        })
+        $("#domba-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/domba.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#babi").click(function () {
+        $("#babi-img").attr({
+            'src': "img/babi2.png"
+        });
+        $("#babi-text").css({
+            'top': "75%"
+        })
+        $("#babi-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/babi.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+
+    
+    // Hewan buas
+    $("#harimau").click(function () {
+        $("#harimau-img").attr({
+            'src': "img/harimau.png"
+        });
+        $("#harimau-text").css({
+            'top': "75%"
+        })
+        $("#harimau-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/harimau.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#komodo").click(function () {
+        $("#komodo-img").attr({
+            'src': "img/komodo.png"
+        });
+        $("#komodo-text").css({
+            'top': "75%"
+        })
+        $("#komodo-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/komodo.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#singa").click(function () {
+        $("#singa-img").attr({
+            'src': "img/singa.png"
+        });
+        $("#singa-text").css({
+            'top': "75%"
+        })
+        $("#singa-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/singa.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#beruang").click(function () {
+        $("#beruang-img").attr({
+            'src': "img/beruang.png"
+        });
+        $("#beruang-text").css({
+            'top': "75%"
+        })
+        $("#beruang-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/beruang.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#serigala").click(function () {
+        $("#serigala-img").attr({
+            'src': "img/serigala.png"
+        });
+        $("#serigala-text").css({
+            'top': "75%"
+        })
+        $("#serigala-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/serigala.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+});
